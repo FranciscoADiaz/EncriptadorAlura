8ce792d8f8d5cffd6ef29db03bb45ff7768f5b7a
franciscoadiaz <arieldiazx@outlook.com.ar>
2023-05-16 02:03:52 -0300

    se corrigió modo oscuro, pero todavia falta :v

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
   
  /*FUNCIÓN ENCRIPTAR*/
 function encriptar() {
-  // Obtiene el valor del texto ingresado en el elemento HTML con la clase "text-area" y lo convierte a minúsculas
+  // Obtiene el valor del texto ingresado en el elemento ".text-area" y lo convierte a minúsculas
   const texto = document.querySelector(".text-area").value.toLowerCase();
 
   // Crea un objeto que contiene las letras y sus respectivos valores encriptados
@@ -11,23 +11,21 @@ function encriptar() {
   // y la función de reemplazo proporcionada por el objeto "valores"
   const textoEncriptado = texto.replace(/[eiaou]/img, letra => valores[letra]);
 
-  // Actualiza el contenido del elemento HTML con el id "mensaje" con el texto encriptado
+  // Escribe el textoEncriptado dentro del elemento "#mensaje"
   document.querySelector("#mensaje").innerHTML = textoEncriptado;
 
-  // Borra el texto en el área de texto HTML con la clase "text-area"
+  // Borra el texto dentro del elemento ".text-area"
   document.querySelector(".text-area").value = "";
 
-  // Quita la imagen de fondo del elemento HTML con el id "mensaje"
+  // Quita la imagen de fondo del elemento "#mensaje"
   document.querySelector("#mensaje").style.backgroundImage = "none";
 
-
-
-  //mensaje de sugerencia si se intenta encriptar con el campo vacío
+  // Mensaje de sugerencia si se intenta encriptar con el campo vacío
   if (texto === "") {
-    //el mensaje que muestra
+    // El valor del placeholder si el campo está vacío
     document.querySelector(".text-area").setAttribute("placeholder", "Debe ingresar un texto antes de encriptar");
     
-    //Restaurar el placeholder original después de 1.5 segundos
+    // Restaura el placeholder original después de 1.5 segundos
     setTimeout(function() {
       document.querySelector(".text-area").setAttribute("placeholder", "Ingrese su texto aquí");
     }, 1500);
@@ -42,20 +40,20 @@ function desencriptar() {
   // Obtiene el valor del texto ingresado en el elemento HTML con la clase "text-area" y lo convierte a minúsculas
   const textoEncriptado = document.querySelector(".text-area").value.toLowerCase();
 
-  // Crea un objeto que contiene los valores encriptados y sus respectivas letras desencriptadas
+  // Crea un objeto que contiene los valores encriptados y sus respectivas letras
   const valores = { "enter": "e", "imes": "i", "ai": "a", "ober": "o", "ufat": "u" };
 
   // Reemplaza los valores encriptados "enter", "imes", "ai", "ober" y "ufat" en el textoEncriptado
   // utilizando una expresión regular y la función de reemplazo proporcionada por el objeto "valores"
   const textoDesencriptado = textoEncriptado.replace(/enter|imes|ai|ober|ufat/g, valor => valores[valor]);
 
-  // Actualiza el contenido del elemento HTML con el id "mensaje" con el texto desencriptado
+  // Actualiza el contenido del elemento "#mensaje" con el texto desencriptado
   document.querySelector("#mensaje").innerHTML = textoDesencriptado;
 
-  // Borra el texto en el área de texto HTML con la clase "text-area"
+  // Borra el texto dentro del elemento "text-area"
   document.querySelector(".text-area").value = "";
 
-  // Quita la imagen de fondo del elemento HTML con el id "mensaje"
+  // Quita la imagen de fondo del elemento "#mensaje"
   document.querySelector("#mensaje").style.backgroundImage = "none";
 }
 
@@ -63,10 +61,9 @@ function desencriptar() {
 
 /*BOTON COPIAR*/
 function copiar() {
-  // Obtiene el contenido del elemento HTML con el id "mensaje"
+  // Obtiene el contenido del elemento "#mensaje"
   let mensaje = document.getElementById("mensaje").value;
-
-  // Copia el contenido del mensaje al portapapeles del usuario utilizando la API del portapapeles
+  // Copia el contenido del mensaje al portapapeles de la pc
   navigator.clipboard.writeText(mensaje);
 }
 
@@ -74,10 +71,11 @@ function copiar() {
 
   
 /*SOLO MINÚSCULAS Y SIN ACENTOS*/
+// Se guarda al elemento ".text-area" dentro de una constante
 const textArea = document.querySelector(".text-area");
 
-// Agrega un evento de escucha al elemento HTML con la clase "text-area"
-// para que cuando se ingrese texto, se realice una serie de transformaciones en el texto ingresado
+// Agrega un evento de escucha al elemento ".text-area" para que cuando se ingrese texto, se realice una 
+// serie de transformaciones en el texto ingresado
 textArea.addEventListener('input', (event) => {
   // Crea una expresión regular que coincide con cualquier caracter que no sea una letra minúscula o un espacio
   const regex = /[^a-z ]/g;
